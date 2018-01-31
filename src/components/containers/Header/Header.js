c56e18528dd400b08d6d337f473dd571dbf9cb30
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2018-01-31 02:46:07 -0800

    added new text prop and prop logic

--- src/components/containers/Header/Header.js
+++ src/components/containers/Header/Header.js
@@ -3,21 +3,26 @@ import styled from 'react-emotion';
 import PropTypes from 'prop-types';
 
 const Container = styled('div')`
-  background-color: red;
+  h1 {
+    font-family: 'Space Mono', monospace;
+  }
 `;
 
-const Header = ({ logo }) => (
+const Header = ({ logo, text }) => (
   <Container>
-    <img src={logo} alt="Logo" />
+    {logo && <img src={logo} alt="Logo" />}
+    {text && <h1>{text}</h1>}
   </Container>
 );
 
 Header.propTypes = {
   logo: PropTypes.string,
+  text: PropTypes.string,
 };
 
 Header.defaultProps = {
   logo: '',
+  text: '',
 };
 
 export default Header;
