ffdd1c7e31b4bf358240b3797c79ff454b15f1a0
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2018-01-31 02:37:49 -0800

    added space-mono font

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { injectGlobal } from 'emotion';
+
 import App from './App';
+
+injectGlobal`
+  @font-face {
+    src: url('https://fonts.googleapis.com/css?family=Space+Mono|Work+Sans');
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
