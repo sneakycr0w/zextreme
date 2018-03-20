cd2bea48191f66baefb42424d39dbcd5b3c11a3d
zach <zach@isolary.com>
2018-03-20 13:09:03 -0700

    eslint update & modified navigation/header

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 import styled from 'react-emotion';
-import Header from '../components/Header/Header';
+import logo from '../assets/logo.png';
 
-const Container = styled('div')`
-  max-width: 945px;
+const Footer = styled('div')`
+  height: 10vh;
 `;
 
+const Body = styled('div')`
+  min-height: 80vh;
+`;
+
+const Header = styled(Row)`
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  margin-top: 10px;
+`
+
+const Logo = styled('img')`
+  width: 50%;
+  height: auto;
+`
+
 const App = () => (
-  <Container>
-    <Header text="Z Extreme Productions" />
-    <p>This website is currently under construction</p>
-  </Container>
+  <Grid>
+    <Header>
+      <Logo src={logo} alt=""/>
+    </Header>
+    <Body>
+      <Row>
+        <Col md={4}><i className="fas fa-users fa-9x"></i></Col>
+        <Col md={4}><i className="fas fa-book fa-9x"></i></Col>
+        <Col md={4}><i className="fab fa-accessible-icon fa-9x"></i></Col>
+      </Row>
+    </Body>
+    <Footer>
+      <p>Footer placeholder</p>
+    </Footer>
+  </Grid>
 );
 
 export default App;
