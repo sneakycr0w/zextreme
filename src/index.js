b73860029bcf7cd4df2e9cd85b666551a7d5a828
Zachary E Sohovich <zacharyesohovich@gmail.com>
2018-07-07 17:50:45 -0700

    added src dir

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { injectGlobal } from 'react-emotion';
+import App from './App';
+
+injectGlobal`
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Lato', sans-serif;
+  }
+`;
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
